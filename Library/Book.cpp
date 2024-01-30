5bed47083c5cd1270b94319393227cccff94f778
Mehdi Vakili <mehdivakili79@gmail.com>
2024-01-30 10:05:53 +0330

    Add Book class

---
+++ Library/Book.cpp
@@ -0,0 +1,76 @@
+#include "Book.h"
+
+Book::Book(string t, string g, string d, string a)
+{
+	title = t; genre = g; date = d; author = a;
+}
+
+Book::Book()
+{
+	title = genre = date = author = "";
+}
+
+void Book::setTitle(string input)
+{
+	title = input;
+}
+
+void Book::setGenre(string input)
+{
+	genre = input;
+}
+
+void Book::setDate(string input)
+{
+	date = input;
+}
+
+void Book::setAuthor(string input)
+{
+	author = input;
+}
+
+string Book::getTitle() const
+{
+	return this->title;
+}
+
+string Book::getGenre() const
+{
+	return this->genre;
+}
+
+string Book::getDate() const
+{
+	return this->date;
+}
+
+string Book::getAuthor() const
+{
+	return this->author;
+}
+
+Book& Book::operator=(const Book& other)
+{
+	this->title = other.getTitle();
+	this->genre = other.getGenre();
+	this->date = other.getDate();
+	this->author = other.getAuthor();
+	return *this;
+}
+
+bool Book::operator<(const Book& other)
+{
+	if (this->getTitle() < other.getTitle())
+		return true;
+	else
+		return false;
+}
+
+bool Book::operator>=(const Book& other)
+{
+	if (this->getTitle() < other.getTitle())
+		return false;
+	else
+		return true;
+}
---
+++ Library/Book.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class Book
+{
+private:
+	string title;
+	string genre;
+	string date;
+	string author;
+public:
+	Book(string, string, string, string);
+	Book();
+	void setTitle(string);
+	void setGenre(string);
+	void setDate(string);
+	void setAuthor(string);
+	string getTitle() const;
+	string getGenre() const;
+	string getDate() const;
+	string getAuthor() const;
+	Book& operator=(const Book&);
+	bool operator<(const Book&);
+	bool operator>=(const Book&);
+};
