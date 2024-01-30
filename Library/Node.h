fdf47c51b1b2bb831ddcdb66650ea9bd7baf654a
SINA <sina.jahangir.013@gmail.com>
2024-01-30 10:13:31 +0330

    Add Node class

---
+++ Library/Node.h
@@ -0,0 +1,38 @@
+#pragma once
+
+template <typename T>
+class Node
+{
+private:
+	T value;
+	Node* next;
+public:
+	void setValue(T);
+	void setNext(Node*);
+	T getValue();
+	Node* getNext();
+};
+
+template <typename T>
+void Node<T>::setValue(T v)
+{
+	value = v;
+}
+
+template <typename T>
+void Node<T>::setNext(Node* n)
+{
+	next = n;
+}
+
+template <typename T>
+T Node<T>::getValue()
+{
+	return value;
+}
+
+template <typename T>
+Node<T>* Node<T>::getNext()
+{
+	return next;
+}
