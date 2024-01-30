fba7dde05767c0e3baaf730cafe1e8c4905c4453
SINA <sina.jahangir.013@gmail.com>
2024-01-30 10:17:51 +0330

    Update main.cpp

--- Library/main.cpp
+++ Library/main.cpp
@@ -1,10 +1,94 @@
 #include <iostream>
 using namespace std;
 
+#include "Book.h"
+#include "Node.h"
+#include "User.h"
+
+Node<Book>* books;
+Node<User>* users;
+
+void addBook(Book);
+bool removeBook(string);
+void addUser(User);
+bool removeUser(string);
+
 int main()
 {
 
 	cout << "Hello from library !";
 
 	return 0;
+}
+
+///////////////////////////////////////Functions///////////////////////////////////////
+
+void addBook(Book input)
+{
+	Node<Book>* temp = new Node<Book>;
+	temp->setValue(input);
+	temp->setNext(books);
+	books = temp;
+}
+
+bool removeBook(string t)
+{
+	if (books == nullptr)
+		return false;
+
+	Node<Book>* temp = books;
+	if (books->getValue().getTitle() == t)
+	{
+		books = books->getNext();
+		delete temp;
+		return true;
+	}
+	Node<Book>* temp2 = nullptr;
+	while (temp->getNext() != nullptr)
+	{
+		if (temp->getNext()->getValue().getTitle() == t)
+		{
+			temp2 = temp->getNext();
+			temp->setNext(temp->getNext()->getNext());
+			delete temp2;
+			return true;
+		}
+		temp = temp->getNext();
+	}
+	return false;
+}
+
+void addUser(User input)
+{
+	Node<User>* temp = new Node<User>;
+	temp->setValue(input);
+	temp->setNext(users);
+	users = temp;
+}
+
+bool removeUser(string u)
+{
+	if (users == nullptr)
+		return false;
+
+	Node<User>* temp = users;
+	if (users->getValue().getUserName() == u)
+	{
+		users = users->getNext();
+		delete temp;
+		return true;
+	}
+	Node<User>* temp2 = nullptr;
+	while (temp->getNext() != nullptr)
+	{
+		if (temp->getNext()->getValue().getUserName() == u)
+		{
+			temp2 = temp->getNext();
+			temp->setNext(temp->getNext()->getNext());
+			delete temp2;
+			return true;
+		}
+		temp = temp->getNext();
+	}
+	return false;
 }
