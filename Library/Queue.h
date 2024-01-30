8a4c9f38b9ac0700209af298f82d283e45fee761
SINA <sina.jahangir.013@gmail.com>
2024-01-30 10:14:31 +0330

    Add Queue class

---
+++ Library/Queue.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class Queue
+{
+private:
+	static const int MAX = 13;
+	int size;
+	int front;
+	int rear;
+	T queue[MAX];
+public:
+	Queue();
+	int getSize();
+	void enqueue(T);
+	T dequeue();
+};
+
+template<typename T>
+Queue<T>::Queue()
+{
+	size = rear = front = 0;
+}
+
+template<typename T>
+int Queue<T>::getSize()
+{
+	return size;
+}
+
+template <typename T>
+void Queue<T>::enqueue(T input)
+{
+	if (size == MAX)
+		throw exception("queue is full !");
+	queue[rear] = input;
+	rear = (rear + 1) % MAX;
+	size++;
+}
+
+template<typename T>
+T Queue<T>::dequeue()
+{
+	if (size == 0)
+		throw exception("queue is empty !");
+	T temp = queue[front];
+	front = (front + 1) % MAX;
+	size--;
+	return temp;
+}
